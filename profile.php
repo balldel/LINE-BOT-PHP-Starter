a49936f27dab0395ca2495afe1bbf0ac8a654485
Dell <Dell@DellWin10Pc>
2017-02-04 21:17:28 +0700

    init

--- profile.php
+++ profile.php
@@ -1,11 +1,17 @@
 <?php
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('9t0g5+EDaF13gvDHFU9J0XQceLUGhPFsyoF1MfkR7xX3TDwEL3UNdffefZa1+POdZ6TnYgiF/wwc8XW6mjSaX9Eep9apSL35wcJMGXCN+w6no6kn8ZhsrYYvtS34cLa+qLIb3rArpOQAIsqc/WkwVgdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
-$response = $bot->getProfile('U9d261d005044ab0f2cba21b69278a155');
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo $profile['displayName'];
-    echo $profile['pictureUrl'];
-    echo $profile['statusMessage'];
-}
+$access_token = '9t0g5+EDaF13gvDHFU9J0XQceLUGhPFsyoF1MfkR7xX3TDwEL3UNdffefZa1+POdZ6TnYgiF/wwc8XW6mjSaX9Eep9apSL35wcJMGXCN+w6no6kn8ZhsrYYvtS34cLa+qLIb3rArpOQAIsqc/WkwVgdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v2/bot/profile/U9d261d005044ab0f2cba21b69278a155';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
